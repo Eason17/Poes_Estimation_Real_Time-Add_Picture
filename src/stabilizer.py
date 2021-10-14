f7931b8e17293b853aa1dcf70ca245712df07741
Yiqi Huang <62044529+Eason17@users.noreply.github.com>
2021-10-14 22:22:38 +0800

    Update stabilizer.py

--- src/stabilizer.py
+++ src/stabilizer.py
@@ -1,7 +1,3 @@
-"""
-Using Kalman Filter as a point stabilizer to stabilize a 2D point.
-利用卡尔曼滤波器作为点稳定器来稳定二维点。
-"""
 import numpy as np
 import cv2
 
@@ -9,33 +5,24 @@ import cv2
 class Stabilizer:
     """
     Using Kalman filter as a point stabilizer.
-    采用卡尔曼滤波器作为点稳定器。
     """
 
     def __init__(self, state_num=4, measure_num=2, cov_process=0.0001, cov_measure=0.1):
         """Initialization"""
-        # Currently we only support scalar and point, so check user input first.
-        # 目前我们只支持标量和点，所以首先检查用户输入。
         assert state_num == 4 or state_num == 2, "Only scalar and point supported, Check state_num please."
 
-        # Store the parameters. 存储参数。
         self.state_num = state_num
         self.measure_num = measure_num
 
-        # The filter itself. 过滤器本身。
         # 表示转移矩阵维度为state_num，测量矩阵维度为measure_num， 0表示没有控制向量.
         self.filter = cv2.KalmanFilter(state_num, measure_num, 0)
 
-        # Store the state. 存储状态。
         self.state = np.zeros((state_num, 1), dtype=np.float32)  # 建立shape为（state_num，1）的0矩阵
 
-        # Store the measurement result. 存储测量结果。
         self.measurement = np.array((measure_num, 1), np.float32)  # 存放数组的大小为(measure_num, 1)
 
-        # Store the prediction. 存储预测。
         self.prediction = np.zeros((state_num, 1), np.float32)
 
-        # Kalman parameters setup for scalar. 标量的卡尔曼参数设置。
         if self.measure_num == 1:
             self.filter.transitionMatrix = np.array([[1, 1],
                                                      [0, 1]], np.float32)  # 设置转移矩阵。
@@ -48,7 +35,6 @@ class Stabilizer:
             self.filter.measurementNoiseCov = np.array(
                 [[1]], np.float32) * cov_measure  # 设置测量噪声协方差矩阵。
 
-        # Kalman parameters setup for point. 点的卡尔曼参数设置。
         if self.measure_num == 2:
             self.filter.transitionMatrix = np.array([[1, 0, 1, 0],
                                                      [0, 1, 0, 1],
@@ -68,10 +54,8 @@ class Stabilizer:
 
     def update(self, measurement):
         """Update the filter"""
-        # Make kalman prediction  让卡尔曼预测
         self.prediction = self.filter.predict()
 
-        # Get new measurement  获得新的测量
         if self.measure_num == 1:
             self.measurement = np.array([[np.float32(measurement[0])]])
         else:
